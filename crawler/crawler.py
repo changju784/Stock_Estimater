35bb1b7c2844bea63c89d827f5b4566465ee984f
jungin <changju@bu.edu>
2022-12-20 23:08:51 -0500

    a little touch

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
+from bs4.element import Comment
 
 class Crawler:
     @classmethod
@@ -12,14 +13,22 @@ class Crawler:
         cls.instance = cls.__getInstance
         return cls.__instance
 
+    def tag_visible(element):
+        if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+            return False
+        if isinstance(element, Comment):
+            return False
+        return True
+
     def finvizCrawler(self):
         finvizURL = 'http://finviz.com/news.ashx'
         bloombergURL = 'http://www.bloomberg.com/markets/economics'
         req = Request(finvizURL, headers={'User-Agent': 'Mozilla/5.0'})
         html = urlopen(req).read()
-        bsObject = BeautifulSoup(html, 'html.parser')
-        print(bsObject.head)
-        print(bsObject.head.find('meta', {'name':'description'}).get('content'))
+        soup = BeautifulSoup(html, 'html.parser').body
+        # print(bsObject.head.find('meta', {'name':'description'}).get('content'))
+        [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
+
 
 
 if __name__ == '__main__':

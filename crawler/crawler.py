6896f6faed64ec9f5d9380e51ccde99a8a82136c
jungin <changju@bu.edu>
2023-01-14 19:31:38 -0500

    more formatting and adding ratings

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,7 +1,8 @@
+import pandas as pd
 from datetime import datetime
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
-import pandas as pd
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 class Crawler:
     @classmethod
@@ -15,9 +16,11 @@ class Crawler:
         return cls.__instance
 
     def finvizCrawler(self):
-        # Returns time & news title dataframe crawled from finviz url.
+        '''
+        Returns time & news title dataframe crawled from finviz url.
+        :return: dataframe
+        '''
         finvizURL = 'http://finviz.com/news.ashx'
-        # bloombergURL = 'http://www.bloomberg.com/markets/economics'
         req = Request(finvizURL, headers={'User-Agent': 'Mozilla/5.0'})
         html = urlopen(req).read()
         soup = BeautifulSoup(html, 'html.parser')
@@ -29,11 +32,24 @@ class Crawler:
                     tables.append(tableData)
         df = pd.DataFrame(tables[1:], columns=['', 'Time', 'Title'])
         df = df.iloc[:, 1:]
-        df = df[df['Time'].map(len) == 7]
 
+        # Preprocess Title
+        df = df[df['Title'] != "Loading…"]
+
+        # Format Time
+        df = df[df['Time'].map(len) == 7]
         now = datetime.today().strftime('%Y-%m-%d')
-        df["Time"] = now + " " + df["Time"]
+        df['Time'] = now + ' ' + df['Time']
+        df = self.usePreTrainedModel(df)
         return df
+
+    def usePreTrainedModel(self, df):
+        df['Rate'] = ''
+        sia = SentimentIntensityAnalyzer()
+        for index, row in df.iterrows():
+            row['Rate'] = sia.polarity_scores(row['Title'])['compound']
+        return df
+
 
     def saveCSV(self, df):
         filePath = "../docs/finviz_articles.csv"
